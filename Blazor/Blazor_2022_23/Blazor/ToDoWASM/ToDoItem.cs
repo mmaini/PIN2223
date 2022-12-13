f6ede6cba96f636a929974ffb694dcd0731df585
mmaini <mariza.maini@itsistemi.com>
2022-12-13 20:20:53 +0100

    Blazor

---
+++ Blazor/Blazor_2022_23/Blazor/ToDoWASM/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoWASM
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
